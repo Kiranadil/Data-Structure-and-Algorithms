a40e97f11f3462118c9f260a40efc82b7b2d05ce
Kiranadil <kiranadil10@gmail.com>
2021-06-13 15:02:45 +0500

    work with indentation

--- loops/For loop/app1.js
+++ loops/For loop/app1.js
@@ -5,13 +5,13 @@ the name of rabbit, print out ‘this is rabbit’.
 When you don’t get the name of a rabbit, print 
 ‘this is not a rabbit’.
 */
-let petAnimals= ["cat", "rabbit", "pigeon", "parrot", "goldfish"];
+let petAnimals = ["cat", "rabbit", "pigeon", "parrot", "goldfish"];
 for (let i = 0; i < petAnimals.length; i++) {
-    if(petAnimals[i]=="rabbit"){
-  console.log("This is rabbit");
+    if (petAnimals[i] == "rabbit") {
+        console.log("This is rabbit");
     }
-    else{
+    else {
         console.log("This is not a rabbit");
     }
-    
+
 }
